2ca3b1a721a693699eb5a162e9751f798a99655d
masterMawi <masterdev.mawi@gmail.com>
2018-04-25 08:59:33 +0200

    zmieniłem imię osoby opisywanej

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-console.log("Max urodził się w 1973 roku.");
-console.log("Max ma ulubione powiedzonko: Carpe diem - chwytaj dzień");
-console.log("Max ma zainteresowania: sci-fi, programowanie, czytanie książek");
-console.log("Max prowadzi zajęcia z programowania: true");
+console.log("Maciek urodził się w 1973 roku.");
+console.log("Maciek ma ulubione powiedzonko: Carpe diem - chwytaj dzień");
+console.log("Maciek ma zainteresowania: sci-fi, programowanie, czytanie książek");
+console.log("Maciek prowadzi zajęcia z programowania: true");
