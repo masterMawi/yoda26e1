ffeed46fd1e87f0efa5f9ad2bccc9b7bc547c476
mawi <masterdev.mawi@gmail.com>
2018-04-25 04:47:26 +0200

    początek pisania programu

---
+++ app.js
@@ -0,0 +1,5 @@
+
+console.log("Max urodził się w 1973 roku.");
+console.log("Max ma ulubione powiedzonko: Carpe diem - chwytaj dzień");
+console.log("Max ma zainteresowania: sci-fi, programowanie, czytanie książek");
+console.log("Max prowadzi zajęcia z programowania: true");
