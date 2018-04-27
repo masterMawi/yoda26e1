87da12edb5412b298114ef412831656582a5cf1a
masterMawi <masterdev.mawi@gmail.com>
2018-04-27 18:53:03 +0200

    w funkcji opisującej osobę zastosowałem dla wygody "szablon łańcuchowy"

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ let opiszOsobe;            // deklaracja zmiennej na funkcję opisującą osobę
 
 // funkcja opisująca osobę
 opiszOsobe = (imie, rokUrodzenia) => {
-  console.log(imie + " urodził(a) się w " + rokUrodzenia + " roku.");
-  console.log(imie + " ma ulubione powiedzonko: jakies-tam-powiedzonko");
-  console.log(imie + " ma zainteresowania: jakies-zainteresowanie1, jakies-zainteresowanie2");
-  console.log(imie + " prowadzi zajęcia z programowania: jeszcze-nie-podana");
+  console.log(`${imie} urodził(a) się w ${rokUrodzenia} roku.`);
+  console.log(`${imie} ma ulubione powiedzonko: jakies-tam-powiedzonko`);
+  console.log(`${imie} ma zainteresowania: jakies-zainteresowanie1, jakies-zainteresowanie2`);
+  console.log(`${imie} prowadzi zajęcia z programowania: jeszcze-nie-podana`);
 }
 
 opiszOsobe("Maciek", 1973);
