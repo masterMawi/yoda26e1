676847dbdd18ff67b234e61ab8a59258be56bd54
mawi <masterdev.mawi@gmail.com>
2018-04-27 22:49:56 +0200

    do funkcji opiszOsobe dodałem argument

--- app.js
+++ app.js
@@ -3,16 +3,15 @@ let imieOsoby;             // deklaracja zmiennej o nazwie imieOsoby
 let opiszOsobe;            // deklaracja zmiennej na funkcję opisującą osobę
 
 // funkcja opisująca osobę
-opiszOsobe = () => {
-  console.log(imieOsoby + " urodził(a) się w ktoryms-tam roku.");
-  console.log(imieOsoby + " ma ulubione powiedzonko: jakies-tam-powiedzonko");
-  console.log(imieOsoby + " ma zainteresowania: jakies-zainteresowanie1, jakies-zainteresowanie2");
-  console.log(imieOsoby + " prowadzi zajęcia z programowania: jeszcze-nie-podana");
+opiszOsobe = (imie) => {
+  console.log(imie + " urodził(a) się w ktoryms-tam roku.");
+  console.log(imie + " ma ulubione powiedzonko: jakies-tam-powiedzonko");
+  console.log(imie + " ma zainteresowania: jakies-zainteresowanie1, jakies-zainteresowanie2");
+  console.log(imie + " prowadzi zajęcia z programowania: jeszcze-nie-podana");
 }
 
 imieOsoby = "Maciek";      // nadanie zmiennej wartości "Maciek"
 opiszOsobe(imieOsoby);
 
 // druga osoba
-imieOsoby = "Tosia";      // nadanie zmiennej wartości "Tosia"
-opiszOsobe(imieOsoby);
+opiszOsobe("Tosia");
