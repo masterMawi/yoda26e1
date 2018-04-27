e1a6397b700c30748ddb1d60da74d4f3c11f32a3
mawi <masterdev.mawi@gmail.com>
2018-04-28 00:26:41 +0200

    w funkcji opiszOsobe zastosowałem literał szablonowy

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // zmienna, której nie można modyfikować zawierająca funkcję opisująca osobę
 const opiszOsobe = (imie) => {
-  console.log(imie + " urodził(a) się w ktoryms-tam roku.");
-  console.log(imie + " ma ulubione powiedzonko: jakies-tam-powiedzonko");
-  console.log(imie + " ma zainteresowania: jakies-zainteresowanie1, jakies-zainteresowanie2");
-  console.log(imie + " prowadzi zajęcia z programowania: jeszcze-nie-podana");
+  console.log(`${imie} urodził(a) się w ktoryms-tam roku.`);
+  console.log(`${imie} ma ulubione powiedzonko: jakies-tam-powiedzonko`);
+  console.log(`${imie} ma zainteresowania: jakies-zainteresowanie1, jakies-zainteresowanie2`);
+  console.log(`${imie} prowadzi zajęcia z programowania: jeszcze-nie-podana`);
 }
 
 // funkcja, która opisze dwie osoby
@@ -11,4 +11,5 @@ const opiszListeOsob = (listaImion) => {
   listaImion.map(opiszOsobe);
 }
 
-opiszListeOsob(["Maciek", "Tosia"]);
+opiszListeOsob(["Maciek", "Tosia", "Matylda", "Milena"]);
+
