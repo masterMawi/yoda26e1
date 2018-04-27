d6a5e02d732875c6ab5d757db500618dfd9f43c8
masterMawi <masterdev.mawi@gmail.com>
2018-04-27 17:56:13 +0200

    dodałem wyświetlanie drugiej osoby

--- app.js
+++ app.js
@@ -6,3 +6,11 @@ console.log(imieOsoby + " urodził(a) się w ktoryms-tam roku.");
 console.log(imieOsoby + " ma ulubione powiedzonko: jakies-tam-powiedzonko");
 console.log(imieOsoby + " ma zainteresowania: jakies-zainteresowanie1, jakies-zainteresowanie2");
 console.log(imieOsoby + " prowadzi zajęcia z programowania: jeszcze-nie-podana");
+
+// druga osoba
+imieOsoby = "Tosia";      // nadanie zmiennej wartości "Maciek"
+
+console.log(imieOsoby + " urodził(a) się w ktoryms-tam roku.");
+console.log(imieOsoby + " ma ulubione powiedzonko: jakies-tam-powiedzonko");
+console.log(imieOsoby + " ma zainteresowania: jakies-zainteresowanie1, jakies-zainteresowanie2");
+console.log(imieOsoby + " prowadzi zajęcia z programowania: jeszcze-nie-podana");
