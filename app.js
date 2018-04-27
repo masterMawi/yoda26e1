858e76c19a357031a6619ea34a2028243a460a7a
masterMawi <masterdev.mawi@gmail.com>
2018-04-27 17:53:16 +0200

    zmieniłem treść informacji o roku urodzenia

--- app.js
+++ app.js
@@ -2,8 +2,7 @@
 let imieOsoby;             // deklaracja zmiennej o nazwie imieOsoby
 imieOsoby = "Maciek";      // nadanie zmiennej wartości "Maciek"
 
-console.log(imieOsoby + " urodził się w ktoryms-tam roku.");
+console.log(imieOsoby + " urodził(a) się w ktoryms-tam roku.");
 console.log(imieOsoby + " ma ulubione powiedzonko: jakies-tam-powiedzonko");
 console.log(imieOsoby + " ma zainteresowania: jakies-zainteresowanie1, jakies-zainteresowanie2");
 console.log(imieOsoby + " prowadzi zajęcia z programowania: jeszcze-nie-podana");
-
