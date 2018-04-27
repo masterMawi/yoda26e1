1863629308107eabb64a0e270720fee0da55a1a8
masterMawi <masterdev.mawi@gmail.com>
2018-04-27 17:48:46 +0200

    dodałem zmienną imieOsoby

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-console.log("Maciek urodził się w ktoryms-tam roku.");
-console.log("Maciek ma ulubione powiedzonko: jakies-tam-powiedzonko");
-console.log("Maciek ma zainteresowania: jakies-zainteresowanie1, jakies-zainteresowanie2");
-console.log("Maciek prowadzi zajęcia z programowania: jeszcze-nie-podana");
+
+let imieOsoby;             // deklaracja zmiennej o nazwie imieOsoby
+imieOsoby = "Maciek";      // nadanie zmiennej wartości "Maciek"
+
+console.log(imieOsoby + " urodził się w ktoryms-tam roku.");
+console.log(imieOsoby + " ma ulubione powiedzonko: jakies-tam-powiedzonko");
+console.log(imieOsoby + " ma zainteresowania: jakies-zainteresowanie1, jakies-zainteresowanie2");
+console.log(imieOsoby + " prowadzi zajęcia z programowania: jeszcze-nie-podana");
 
