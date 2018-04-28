b9659dae5debda55aed4acfff0e43cb7d055040a
mawi <masterdev.mawi@gmail.com>
2018-04-28 07:10:07 +0200

    dodałem do opiszOsobe wyświetlanie indeksu osoby

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 // zmienna, której nie można modyfikować zawierająca funkcję opisująca osobę
-const opiszOsobe = (imie) => {
+const opiszOsobe = (imie, index) => {
+  if(index) {
+    console.log(`${index}: ${imie}`);
+  }
   console.log(`${imie} urodził(a) się w ktoryms-tam roku.`);
   console.log(`${imie} ma ulubione powiedzonko: jakies-tam-powiedzonko`);
   console.log(`${imie} ma zainteresowania: jakies-zainteresowanie1, jakies-zainteresowanie2`);
@@ -10,6 +13,8 @@ const opiszOsobe = (imie) => {
 const opiszListeOsob = (listaImion) => {
   listaImion.map(opiszOsobe);
 }
+
+opiszOsobe("Max");
 
 opiszListeOsob(["Maciek", "Tosia", "Matylda", "Milena"]);
 
